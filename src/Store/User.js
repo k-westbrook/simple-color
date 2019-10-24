7fe85ff07d8a092b4463eb54fb8eef5142a9f063
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-24 14:54:41 -0700

    updated thunk

--- src/Store/User.js
+++ src/Store/User.js
@@ -15,7 +15,7 @@ const userObject = {};
  * ACTION CREATORS
  */
 
-const addUser = user => ({ type: ADD_USER, user })
+const addUser = (user) => ({ type: ADD_USER, user })
 
 
 /**
@@ -25,8 +25,23 @@ export const registerUser = (email, password) => async dispatch => {
 
   try {
     let response = await axios.post("https://pgqrxh9ys4.execute-api.us-west-1.amazonaws.com/Prod/", { email, password });
+
+    dispatch(addUser(response));
   } catch (error) {
     console.log(error);
   }
 
+}
+
+
+/**
+ * REDUCER
+ */
+export default function (state = userObject, action) {
+  switch (action.type) {
+    case ADD_USER:
+      return action.user
+    default:
+      return state
+  }
 }
