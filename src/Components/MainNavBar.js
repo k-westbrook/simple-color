8c1d0f20efd163a65f2f01b2c3bde654148540f4
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-25 16:31:02 -0700

    updated link for logout

--- src/Components/MainNavBar.js
+++ src/Components/MainNavBar.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { logoutUser } from '../Store/User';
 
 const MainNavBar = () => {
+
   return (
     <nav>
       <Link to="/home">Home</Link>
       <Link to="login">Login</Link>
+      <Link to="/home">Logout</Link>
     </nav>
 
   )
 }
+const mapDispatch = dispatch => {
+  return {
+    logout: () => dispatch(logoutUser())
+  }
+}
 
-export default MainNavBar;
+
+export default connect(null, mapDispatch)(MainNavBar)
+
+
