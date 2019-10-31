b0612547b9f1978803fd5d605a8aef2b9d49b992
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-31 11:10:20 -0700

    added event reducer to event store

--- src/Store/Event.js
+++ src/Store/Event.js
@@ -5,6 +5,7 @@ import history from '../history'
  * ACTION TYPES
  */
 const GET_ALL_EVENTS = 'GET_ALL_EVENTS';
+const ADD_EVENT = 'ADD_EVENT';
 const GET_SPECIFIC_EVENT = 'GET_SPECIFIC_EVENT';
 const REMOVE_EVENT = 'REMOVE_EVENT';
 
@@ -22,6 +23,7 @@ const eventObject = {
  */
 
 const getEvents = (events) => ({ type: GET_ALL_EVENTS, events })
+const addEvent = (event) => ({ type: ADD_EVENT, event })
 const getEvent = (event) => ({ type: GET_SPECIFIC_EVENT, event })
 const removeEvent = (event) => ({ type: REMOVE_EVENT, event })
 
@@ -29,3 +31,16 @@ const removeEvent = (event) => ({ type: REMOVE_EVENT, event })
 /**
  * THUNK CREATORS
  */
+
+
+/**
+* REDUCER
+*/
+export default function (state = eventObject, action) {
+  switch (action.type) {
+    case ADD_EVENT:
+      return action.user
+    default:
+      return state
+  }
+}
