defb7bc08a28c0e9ba1835a283690e072f147d36
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-30 22:27:37 -0700

    form to search for yelp

--- src/Components/AddEvent.js
+++ src/Components/AddEvent.js
@@ -3,20 +3,41 @@ import axios from 'axios'
 
 
 export default class AddEvent extends React.Component {
-  async test() {
-    let results = await axios.post("https://b36yrfjg83.execute-api.us-west-1.amazonaws.com/Prod", { event: "steak" });
 
-    console.log(results);
+  constructor() {
+    super();
+    this.state =
+      {
+        searchResults: []
+      }
+    this.getYelpSearchResults = this.getYelpSearchResults.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  async getYelpSearchResults(search) {
+    let results = await axios.post("https://b36yrfjg83.execute-api.us-west-1.amazonaws.com/Prod", { search: search });
+    this.setState({ searchResults: results.data.body.eventSearchResults })
 
   }
 
-  render() {
+  handleSubmit(evt) {
+    evt.preventDefault();
+    let searchItem = evt.target.search.value;
+    this.getYelpSearchResults(searchItem);
+  }
 
+
+  render() {
+    console.log(this.state)
     return (
       <div>
         <h4>Add Event</h4>
-
-
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="search"></label>
+            <input name="search" type="text" />
+            <button>Search</button>
+          </form>
+        </div>
       </div>
     )
   }
--- src/Components/EventsContainer.js
+++ src/Components/EventsContainer.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default class EventsContainer extends React.Component {
 
   render() {
-    this.test();
+
     return (
       <div>
         <h4>My eats</h4>
