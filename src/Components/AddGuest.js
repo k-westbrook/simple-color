badeb287b1db6964ad92213f3e3cf1af0f3fa573
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-10 21:26:54 -0800

    add guest component

---
+++ src/Components/AddGuest.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default class AddGuest extends React.Component {
+
+
+  render() {
+
+    return (
+      <div>
+        <h4>Welcome to Simply Eat</h4>
+
+
+      </div>
+    )
+  }
+}
